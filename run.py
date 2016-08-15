22adff5b64f27514df963bd22e7506d78a5b5435
Maneesh Apte <maneesh@kanjoya.com>
2016-08-14 23:05:33 -0700

    time

--- run.py
+++ run.py
@@ -62,7 +62,7 @@ class Config(object):
             'trigger': 'cron',
             # 'day_of_week': 6,
             # 'hour': 20, #7pm
-            'minute': 6
+            'minute': 7
         }
     ]
     SCHEDULER_VIEWS_ENABLED = True
