ac4a64c2279d19e0a9b7fa2d07f49a37c800da6a
Maneesh Apte <maneesh@kanjoya.com>
2016-12-25 13:27:13 -0600

    squashing git history

--- run.py
+++ run.py
@@ -2,7 +2,6 @@
 import os
 from flask import Flask, request, redirect
 import twilio.twiml
-from flask_apscheduler import APScheduler
 from twilio.rest import TwilioRestClient
 import random
 
@@ -13,12 +12,12 @@ client = TwilioRestClient(account_sid, auth_token)
 
 DEFAULT_MESSAGE = "Hey I'm texting you but not sure what the message should be"
 MANEESH = "+17013615368"
-TED = "+17012039811"
+TED = "+17013531729"
 
 
 def send_message(message=DEFAULT_MESSAGE):
 	message = client.messages.create(
-		body=message,
+		body="heyyy",
 	    to=MANEESH,
 	    from_=TED, 
 	    )
@@ -28,8 +27,8 @@ def call_parents_reminder():
 	options = [
 		"Hey make sure to call the parents",
 		"Don't forget to call the rents",
-		"Boo boo and bow bow probably want to talk. Call home!",
-		"You're a loser call your mom"
+		"Teddy and Sunny want to talk. Call home!",
+		"You're a loser call mom"
 	]
 	message = random.choice(options) # nifty random choice
 	send_message(message)
@@ -42,39 +41,40 @@ class Config(object):
             'func': 'run:call_parents_reminder',
             # 'args': (1, 2),
             'trigger': 'cron',
-            'day_of_week': '0-4',
-            'hour': 19, #7pm
-            'minute': 30
+            'day_of_week': 'mon-fri',
+            'hour': '19', #7pm
+            'minute': '30'
         },
         {
             'id': 'call_parents_reminder_weekend',
             'func': 'run:call_parents_reminder',
             # 'args': (1, 2),
             'trigger': 'cron',
-            'day_of_week': '5-6',
-            'hour': 16, #4pm
-            'minute': 0
+            'day_of_week': 'sat-sun',
+            'hour': '16', #4pm
         },
         {
             'id': 'call_parents_reminder_test',
             'func': 'run:call_parents_reminder',
             # 'args': (1, 2),
             'trigger': 'cron',
-            # 'day_of_week': 6,
-            # 'hour': 20, #7pm
-            'minute': 51
+            'day_of_week': 'sat-sun',
+            'hour': '19', #4pm
         }
     ]
     SCHEDULER_VIEWS_ENABLED = True
 
 
 app = Flask(__name__)
-app.config.from_object(Config())
-if not app.debug or os.environ.get("WERKZEUG_RUN_MAIN") == "true":
-    # The app is not in debug mode or we are in the reloaded process
-	scheduler = APScheduler()
-	scheduler.init_app(app)
-	scheduler.start()
+app.use_reloader = False
+# app.debug=True
+# if not app.debug or os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+#     # The app is not in debug mode or we are in the reloaded process
+#     app.config.from_object(Config())
+#     scheduler = APScheduler()
+#     scheduler.init_app(app)
+#     print "starting scheduler"
+#     scheduler.start()
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -87,9 +87,8 @@ def respond():
 
 
 if __name__ == "__main__":
-
+	print "starting app"
 	app.run(debug=True, use_reloader=False)
-
 	# app.run(debug=True)
 
 
